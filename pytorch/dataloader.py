56f6c068f942f72e0ce30285f7f50e1580ac7363
Syed Abdul Mateen <72622483+SyedMa3@users.noreply.github.com>
2023-11-21 17:15:17 +0530

    modified dataloader to support cholect50 challenge

--- pytorch/dataloader.py
+++ pytorch/dataloader.py
@@ -34,7 +34,9 @@ Copyright 2021 The Research Group CAMMA Authors All Rights Reserved.
 """
 
 import os
+import json
 import random
+import torch
 import numpy as np
 from PIL import Image
 import torchvision.transforms as transforms
@@ -44,9 +46,10 @@ from torch.utils.data import Dataset, ConcatDataset, DataLoader
 class CholecT50():
     def __init__(self, 
                 dataset_dir, 
-                dataset_variant="cholect45-crossval",
+                dataset_variant="cholect50-crossval",
                 test_fold=1,
-                augmentation_list=['original', 'vflip', 'hflip', 'contrast', 'rot90']):
+                augmentation_list=['original', 'vflip', 'hflip', 'contrast', 'rot90'],
+                normalize=True):
         """ Args
                 dataset_dir : common path to the dataset (excluding videos, output)
                 list_video  : list video IDs, e.g:  ['VID01', 'VID02']
@@ -56,15 +59,17 @@ class CholecT50():
                 batch_size: int, 
                 shuffle: True or False
             Return
-                tuple ((image), (tool_label, verb_label, target_label, triplet_label))
+                tuple ((image), (tool_label, verb_label, target_label, triplet_label, phase_label))
         """
+        self.normalize   = normalize
         self.dataset_dir = dataset_dir
         self.list_dataset_variant = {
             "cholect45-crossval": "for CholecT45 dataset variant with the official cross-validation splits.",
-            "cholect50-crossval": "for CholecT50 dataset variant with the official cross-validation splits",
+            "cholect50-crossval": "for CholecT50 dataset variant with the official cross-validation splits (recommended)",
             "cholect50-challenge": "for CholecT50 dataset variant as used in CholecTriplet challenge",
             "cholect50": "for the CholecT50 dataset with original splits used in rendezvous paper",
             "cholect45": "a pointer to cholect45-crossval",
+            "cholect50-subset": "specially created for EDU4SDS summer school"
         }
         assert dataset_variant in self.list_dataset_variant.keys(), print(dataset_variant, "is not a valid dataset variant")
         video_split  = self.split_selector(case=dataset_variant)
@@ -94,6 +99,7 @@ class CholecT50():
         self.build_train_dataset(trainform)
         self.build_val_dataset(trainform)
         self.build_test_dataset(testform)
+        self.target_transform = self.to_binary
     
     def list_dataset_variants(self):
         print(self.list_dataset_variant)
@@ -111,7 +117,7 @@ class CholecT50():
             'cholect50-challenge': {
                 'train': [1, 15, 26, 40, 52, 79, 2, 27, 43, 56, 66, 4, 22, 31, 47, 57, 68, 23, 35, 48, 60, 70, 13, 25, 49, 62, 75, 8, 12, 29, 50, 78, 6, 51, 10, 73, 14, 32, 80, 42],
                 'val':   [5, 18, 36, 65, 74],
-                'test':  [92, 96, 103, 110, 111]
+                'test':  [68, 70, 73, 74, 75]
             },
             'cholect45-crossval': {
                 1: [79,  2, 51,  6, 25, 14, 66, 23, 50,],
@@ -135,45 +141,50 @@ class CholecT50():
 
     def transform(self):
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        op_test   = [transforms.Resize((256, 448)), transforms.ToTensor(), normalize,]
-        op_train  = [transforms.Resize((256, 448))] + self.augmentation_list + [transforms.Resize((256, 448)), transforms.ToTensor(), normalize,]
+        op_test   = [transforms.Resize((256, 448)), transforms.ToTensor(), ]
+        op_train  = [transforms.Resize((256, 448))] + self.augmentation_list + [transforms.Resize((256, 448)), transforms.ToTensor()]
+        if self.normalize:
+            op_test.append(normalize)
+            op_train.append(normalize)
         testform  = transforms.Compose(op_test)
         trainform = transforms.Compose(op_train)
         return trainform, testform
+    
+    def to_binary(self, label_list):
+        outputs = []
+        for label in label_list:
+            label = torch.tensor(label).bool().int()
+            outputs.append(label)
+        return outputs
+
 
     def build_train_dataset(self, transform):
         iterable_dataset = []
         for video in self.train_records:
-            dataset = T50(img_dir = os.path.join(self.dataset_dir, 'data', video), 
-                        triplet_file = os.path.join(self.dataset_dir, 'triplet', '{}.txt'.format(video)), 
-                        tool_file = os.path.join(self.dataset_dir, 'instrument', '{}.txt'.format(video)),  
-                        verb_file = os.path.join(self.dataset_dir, 'verb', '{}.txt'.format(video)),  
-                        target_file = os.path.join(self.dataset_dir, 'target', '{}.txt'.format(video)), 
-                        transform=transform)
+            dataset = T50(img_dir = os.path.join(self.dataset_dir, 'videos', video), 
+                          label_file = os.path.join(self.dataset_dir, 'labels', '{}.json'.format(video)),
+                          transform=transform,
+                          target_transform=self.target_transform)
             iterable_dataset.append(dataset)
         self.train_dataset = ConcatDataset(iterable_dataset)
 
     def build_val_dataset(self, transform):
         iterable_dataset = []
         for video in self.val_records:
-            dataset = T50(img_dir = os.path.join(self.dataset_dir, 'data', video), 
-                        triplet_file = os.path.join(self.dataset_dir, 'triplet', '{}.txt'.format(video)), 
-                        tool_file = os.path.join(self.dataset_dir, 'instrument', '{}.txt'.format(video)),  
-                        verb_file = os.path.join(self.dataset_dir, 'verb', '{}.txt'.format(video)),  
-                        target_file = os.path.join(self.dataset_dir, 'target', '{}.txt'.format(video)), 
-                        transform=transform)
+            dataset = T50(img_dir = os.path.join(self.dataset_dir, 'videos', video), 
+                          label_file = os.path.join(self.dataset_dir, 'labels', '{}.json'.format(video)),
+                          transform=transform,
+                          target_transform=self.target_transform)
             iterable_dataset.append(dataset)
         self.val_dataset = ConcatDataset(iterable_dataset)
 
     def build_test_dataset(self, transform):
         iterable_dataset = []
         for video in self.test_records:
-            dataset = T50(img_dir = os.path.join(self.dataset_dir, 'data', video), 
-                triplet_file = os.path.join(self.dataset_dir, 'triplet', '{}.txt'.format(video)), 
-                tool_file = os.path.join(self.dataset_dir, 'instrument', '{}.txt'.format(video)),  
-                verb_file = os.path.join(self.dataset_dir, 'verb', '{}.txt'.format(video)),  
-                target_file = os.path.join(self.dataset_dir, 'target', '{}.txt'.format(video)), 
-                transform=transform)
+            dataset = T50(img_dir = os.path.join(self.dataset_dir, 'videos', video), 
+                          label_file = os.path.join(self.dataset_dir, 'labels', '{}.json'.format(video)), 
+                          transform=transform,
+                          target_transform=self.target_transform)
             iterable_dataset.append(dataset)
         self.test_dataset = iterable_dataset
         
@@ -182,32 +193,52 @@ class CholecT50():
 
     
 class T50(Dataset):
-    def __init__(self, img_dir, triplet_file, tool_file, verb_file, target_file, transform=None, target_transform=None):
-        self.triplet_labels = np.loadtxt(triplet_file, dtype=np.int, delimiter=',',)
-        self.tool_labels = np.loadtxt(tool_file, dtype=np.int, delimiter=',',)
-        self.verb_labels = np.loadtxt(verb_file, dtype=np.int, delimiter=',',)
-        self.target_labels = np.loadtxt(target_file, dtype=np.int, delimiter=',',)
+    def __init__(self, img_dir, label_file, transform=None, target_transform=None):
+        label_data = json.load(open(label_file, "rb"))
+        self.label_data = label_data["annotations"]
+        self.frames = self.label_data.keys()
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
         
     def __len__(self):
-        return len(self.triplet_labels)
+        return len(self.frames)
+
+    def get_binary_labels(self, labels):
+        tool_label = np.zeros([6])
+        verb_label = np.zeros([10])
+        target_label = np.zeros([15])
+        triplet_label = np.zeros([100])
+        phase_label = np.zeros([100])
+        for label in labels:
+            triplet = label[0:1]
+            if triplet[0] != -1.0:
+                triplet_label[triplet[0]] += 1
+            tool = label[1:7]
+            if tool[0] != -1.0:
+                tool_label[tool[0]] += 1
+            verb = label[7:8]
+            if verb[0] != -1.0:
+                verb_label[verb[0]] += 1
+            target = label[8:14]  
+            if target[0] != -1.0:   
+                target_label[target[0]] += 1       
+            phase = label[14:15]
+            if phase[0] != -1.0:
+                phase_label[phase[0]] += 1
+        return (triplet_label, tool_label, verb_label, target_label, phase_label)
     
-    def __getitem__(self, index):
-        triplet_label = self.triplet_labels[index, 1:]
-        tool_label = self.tool_labels[index, 1:]
-        verb_label = self.verb_labels[index, 1:]
-        target_label = self.target_labels[index, 1:]
-        basename = "{}.png".format(str(self.triplet_labels[index, 0]).zfill(6))
+    def __getitem__(self, index):        
+        labels   = self.label_data["annotations"][self.frames[index]]
+        basename = "{}.png".format(str(self.frames[index]).zfill(6))
         img_path = os.path.join(self.img_dir, basename)
         image    = Image.open(img_path)
+        labels   = self.get_binary_labels(labels)
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
-            triplet_label = self.target_transform(triplet_label)
-        return image, (tool_label, verb_label, target_label, triplet_label)
-
+            labels = self.target_transform(labels)
+        return image, labels
 
 if __name__ == "__main__":
     print("Refers to https://github.com/CAMMA-public/cholect45 for the usage guide.")
